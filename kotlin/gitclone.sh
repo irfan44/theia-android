7cbd873d931fc1baa4033bbc8f42cf5ddc299af6
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2021-03-10 22:53:11 +0700

    Add example project for default

--- kotlin/gitclone.sh
+++ kotlin/gitclone.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "Selected project : $1"
-git clone $1 /home/project
+if [ ! -z $1 ] ; then
+  echo "Selected project : $1"
+  git clone $1 /home/project
+else
+  echo "Example project"
+  git clone https://github.com/irfan44/example-android-project.git /home/project
+fi
